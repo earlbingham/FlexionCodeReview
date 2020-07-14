edb687efd689904ac65d8bdaef71b8712e3a1582
Earl Bingham <earlbingham@gmail.com>
2020-07-13 17:09:24 -0700

    added more regression tests

---
+++ tests/volumes/addVolumeSRs.cmd
@@ -0,0 +1,25 @@
+
+curl -d "@celsiusFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@celsiusKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@celsiusRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@fahrenheitCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@fahrenheitKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@fahrenheitRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@kelvinCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@kelvinFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@kelvinRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@rankineCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@rankineFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@rankineKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
