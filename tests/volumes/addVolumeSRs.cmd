594c139951cd0b40e4386f9f40767ff14cd3f42b
Earl Bingham <earlbingham@gmail.com>
2020-07-14 07:44:39 -0700

    added volume regression tests

--- tests/volumes/addVolumeSRs.cmd
+++ tests/volumes/addVolumeSRs.cmd
@@ -1,25 +1,61 @@
 
-curl -d "@celsiusFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicfeetCubicinches.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@celsiusKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicfeetCups.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@celsiusRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicfeetGallons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@fahrenheitCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicfeetLiters.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@fahrenheitKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicfeetTablespoons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@fahrenheitRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicinchesCubicfeet.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@kelvinCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicinchesCups.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@kelvinFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicinchesGallons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@kelvinRankine.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicinchesLiters.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@rankineCelsius.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cubicinchesTablespoons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@rankineFahrenheit.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cupsCubicfeet.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
-curl -d "@rankineKelvin.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+curl -d "@cupsCubicinches.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@cupsGallons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@cupsLiters.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@cupsTablespoons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@gallonsCubicfeet.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@gallonsCubicinches.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@gallonsCups.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@gallonsLiters.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@gallonsTablespoons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@litersCubicfeet.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@litersCubicinches.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@litersCups.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@litersGallons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@litersTablespoons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@tablespoonsCubicfeet.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@tablespoonsCubicinches.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@tablespoonsCups.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@tablespoonsGallons.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
+
+curl -d "@tablespoonsLiters.json" -H "Content-Type: application/json" -X POST http://localhost:3000/api/students/cli
 
